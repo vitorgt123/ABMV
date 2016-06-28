6b12672776017161b9b5c3efcfacb1c44baa72d5
Andre Mauricio <andredigde@gmail.com>
2016-06-28 13:24:00 -0300

    Insert das faltas.

--- src/ABMVSolucoes/src/main/java/abmv/JSF/AulaBean.java
+++ src/ABMVSolucoes/src/main/java/abmv/JSF/AulaBean.java
@@ -5,11 +5,18 @@
  */
 package abmv.JSF;
 
+import abmv.CRUD.CRUDAula;
 import abmv.CRUD.CRUDConteudo;
 import abmv.CRUD.CRUDDisciplina;
+import abmv.CRUD.CRUDMatricula;
+import abmv.Entidade.Aulas;
+import abmv.Entidade.AulasPK;
 import abmv.Entidade.Conteudo;
 import abmv.Entidade.ConteudoPK;
 import abmv.Entidade.Disciplina;
+import abmv.Entidade.Matricula;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -25,6 +32,8 @@ public class AulaBean {
     private String dia;
     private String descricao;
     private int qtda;
+    private static List<Matricula> matrs;
+    private static List<Aulas> aulas;
     /**
      * Creates a new instance of AulaBean
      */
@@ -62,6 +71,32 @@ public class AulaBean {
     public void setQtda(int qtda) {
         this.qtda = qtda;
     }
+
+    public List<Matricula> getMatrs() {
+        return matrs;
+    }
+
+    public void setMatrs(List<Matricula> matrs) {
+        AulaBean.matrs = matrs;
+    }
+
+    public List<Aulas> getAulas() {
+        return aulas;
+    }
+
+    public void setAulas(List<Aulas> aulas) {
+        AulaBean.aulas = aulas;
+    }
+    
+    public void refreshAlunos(){
+        aulas = new ArrayList();
+        matrs = (List<Matricula>) new CRUDMatricula().getMatriculaByDisciplina(disciplina.getId());
+        for(Matricula mat : matrs){
+            Aulas aula = new Aulas(new AulasPK(mat.getId(),dia),0);
+            aula.setMatricula1(mat);
+            aulas.add(aula);
+        }
+    }
     
     public void salvar(){
         disciplina.setQtdeaulas(disciplina.getQtdeaulas() + qtda);
@@ -71,6 +106,10 @@ public class AulaBean {
         cont.setDescricao(descricao);
         new CRUDConteudo().persist(cont);
         new CRUDDisciplina().update(disciplina);
+    }
+    
+    public void insertFaltas(Aulas aula){
+        new CRUDAula().persist(aula);
     }
     
 }
